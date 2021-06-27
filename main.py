35f39d088361ea8de2b20f3e4550a49e7a3172e5
Naol Legesse <60758067+Nlege001@users.noreply.github.com>
2021-06-26 23:04:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
-# Importing OpenCV Library for basic image processing functions
 import cv2
-# Numpy for array related functions
 import numpy as np
-# Dlib for deep learning based Modules and face landmark detection
 import dlib
-# face_utils for basic operations of conversion
 from imutils import face_utils
 
-# Initializing the camera and taking the instance
+
 cap = cv2.VideoCapture(0)
 
-# Initializing the face detector and landmark detector
+
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# status marking for current state
+
 sleep = 0
 drowsy = 0
 active = 0
@@ -32,7 +28,7 @@ def blinked(a, b, c, d, e, f):
     down = compute(a, f)
     ratio = up / (2.0 * down)
 
-    # Checking if it is blinked
+    
     if (ratio > 0.25):
         return 2
     elif (ratio > 0.21 and ratio <= 0.25):
@@ -46,7 +42,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
-    # detected face in faces array
+    
     for face in faces:
         x1 = face.left()
         y1 = face.top()
